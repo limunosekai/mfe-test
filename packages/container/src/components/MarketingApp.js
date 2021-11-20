3fc7c97c2efc5b0930a89075a259afad5a3e22e6
limunosekai <powerlsh0103@gmail.com>
2021-11-20 13:11:39 +0900

    Auto Invalidation test

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -4,8 +4,6 @@ import React, { useRef, useEffect } from 'react';
 export default () => {
   const ref = useRef(null);
 
-  console.log('hi');
-
   useEffect(() => {
     mount(ref.current);
   }, []);
